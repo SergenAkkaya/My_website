df38d5ed48a05ead0c1a747e48e2178453c70ce0
SergenAkkaya <akkayasergen384@gmail.com>
2025-10-24 00:11:40 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+//require("../node_modules/jquery/dist/jquery")
+window.$ = $;
+window.jQuery = $;
+jQuery = $;
+    require("../node_modules/bootstrap/dist/js/bootstrap"),
+    require("./js/script");
